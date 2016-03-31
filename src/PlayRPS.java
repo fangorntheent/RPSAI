aae12c7cc0221b56bd4f2d2a7829fa64b70801f4
fangorntheent <gus.lipkin@gmail.com>
2016-03-30 23:36:53 -0400

    Nothing concrete yet

--- src/PlayRPS.java
+++ src/PlayRPS.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by Gus Lipkin on 3/28/2016.
  */
@@ -19,17 +21,40 @@ public class PlayRPS {
             System.out.println("Oops. I farted.");
     }
 
+    private static int combineAlgs(ArrayList algs) {
+        //System.out.println(algs.get(0).getClass().getDeclaredMethods()[0]);
+        //System.out.println(algs.get(0).getClass().getDeclaredMethod("getTotal"));
+        System.out.println(algs.get(0).getClass().getDeclaredFields()[0]);
+        try {
+            System.out.println(algs.get(0).getClass().getDeclaredField("algNumber").getInt(algs.get(0)));
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        System.out.println("oops");
+        return 0;
+    }
+
+    private static void setWeight(ArrayList algs) {
+        int i = 0;
+
+        try {
+            System.out.println(algs.get(i).getClass().getDeclaredMethod("setWeight", algs.get(i).getClass(), i));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
 
         AlgGeneral algGeneral = new AlgGeneral();
         PlayerGeneral playerGeneral = new PlayerGeneral();
         AlgOne algOne = new AlgOne();
+        AlgTwo algTwo = new AlgTwo();
+        ArrayList algList = new ArrayList<>();
+        algList.add(algOne);
+        algList.add(algTwo);
+        setWeight(algList);
 
         algGeneral.algResults.set(0, 1);
-        System.out.println(algGeneral.algResults);
-        System.out.println(algOne.getAlgOne(algGeneral));
-        System.out.println(algGeneral.algResults);
-        System.out.println("hi");
         String s = algGeneral.algResults.toString();
         s = s.replaceAll(", ", "");
         s = s.substring(1, s.length() - 1);
