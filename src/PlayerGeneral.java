8072f44144a19be233eb12aec29284b6d897bda0
fangorntheent <gus.lipkin@gmail.com>
2016-03-30 21:18:52 -0400

    Added public rCount, pCount, and sCount, and a set method

--- src/PlayerGeneral.java
+++ src/PlayerGeneral.java
@@ -5,9 +5,32 @@ import java.util.ArrayList;
  */
 public class PlayerGeneral {
 
+    public int rCount;
+    public int pCount;
+    public int sCount;
     public ArrayList history;
 
     public PlayerGeneral() {
+        rCount = 0;
+        pCount = 0;
+        sCount = 0;
         history = new ArrayList<Integer>();
+    }
+
+    public void setThrowCount(int range) {
+
+        rCount = 0;
+        pCount = 0;
+        sCount = 0;
+
+        for (int i = 0; i <= range; i++) {
+            if ((Integer)(history.get(i)) == 0)
+                rCount++;
+            else if ((Integer)(history.get(i)) == 1)
+                pCount++;
+            else if ((Integer)(history.get(i)) == 2)
+                sCount++;
+        }
+
     }
 }
