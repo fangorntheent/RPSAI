b4f0f6c33541ab0ac28d367ccdf24c0dc4ec5722
fangorntheent <gus.lipkin@gmail.com>
2016-04-02 16:02:58 -0400

    Beginnings of the interface

---
+++ src/AlgInterface.java
@@ -0,0 +1,13 @@
+public interface AlgInterface {
+
+	public int getAlg(AlgGeneral algGeneral);
+	public int getAlg(PlayerGeneral playerGeneral, AlgGeneral algGeneral);
+	
+	public int getTotal();
+	public void setTotal();
+	public void setTotal(int total);
+	public int getWeight();
+	public void setWeight();
+	public void setWeight(int weight);
+
+}
